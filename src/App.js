7912b6cbd4e0f0639ca46fd0d85ead530231d970
Vu Nguyen <v.vunv8@vinid.net>
2020-05-05 11:03:40 +0700

    :seedling: complete
    complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
+import { move } from 'lodash';
+
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 
@@ -37,98 +39,105 @@ const a11yProps = (index) => {
     };
 };
 
-const CloseTabWrapper = () =>
-    forwardRef((props, ref) => (
-        <div ref={ref}>
-            <IconButton onClick={() => console.log('Delete item ')}>
+const DraggableTab = SortableElement(({ label, value, closable, handleDelete }) => (
+    <div style={{ position: 'relative' }}>
+        <Tab label={label} {...a11yProps(value)} value={value} />
+        {closable && (
+            <IconButton onMouseDown={(event) => handleDelete(event, value)}>
                 <CloseIcon fontSize="small" />
             </IconButton>
-        </div>
-    ));
-
-const DraggableTab = SortableElement(({ label, id }) => (
-    <div style={{ position: 'relative' }}>
-        <Tab label={label} {...a11yProps(id)} />
-        <span
-            onMouseDown={(e) => {
-                e.stopPropagation();
-                console.log('Log for debug');
-            }}
-            style={{
-                position: 'absolute',
-                display: 'inline-block',
-                width: '20px',
-                height: '20px',
-                border: '1px solid red',
-                backgroundColor: 'green',
-                color: '#fff',
-                right: 0,
-                top: '5px',
-                zIndex: 10,
-            }}>
-            Delete
-        </span>
+        )}
     </div>
 ));
 
-const DraggableTabContainer = SortableContainer(({ value, tabItems }) => (
+const DraggableTabContainer = SortableContainer(({ value, tabItems, handleDelete }) => (
     <Tabs value={value} indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto">
         {tabItems.map((item, index) => (
-            <DraggableTab key={index} index={index} label={item.title} id={item.id} />
+            <DraggableTab
+                key={index}
+                index={index}
+                label={item.title}
+                value={item.id}
+                closable={tabItems.length > 1}
+                handleDelete={handleDelete}
+            />
         ))}
     </Tabs>
 ));
 
 class App extends PureComponent {
     state = {
-        tabActiveIndex: 0,
+        tabActiveValue: 0,
         tabItems: [
-            { id: 0, title: 'Item 1' },
-            { id: 1, title: 'Item 2' },
-            { id: 2, title: 'Item 3' },
-            { id: 3, title: 'Item 4' },
-            { id: 4, title: 'Item 5' },
-            { id: 5, title: 'Item 6' },
-            { id: 6, title: 'Item 7' },
-        ],
-        tabContents: [
-            { id: 0, title: 'Item 1' },
-            { id: 1, title: 'Item 2' },
-            { id: 2, title: 'Item 3' },
-            { id: 3, title: 'Item 4' },
-            { id: 4, title: 'Item 5' },
-            { id: 5, title: 'Item 6' },
-            { id: 6, title: 'Item 7' },
+            { id: 0, title: 'Item 0' },
+            { id: 1, title: 'Item 1' },
+            { id: 2, title: 'Item 2' },
+            { id: 3, title: 'Item 3' },
+            { id: 4, title: 'Item 4' },
+            { id: 5, title: 'Item 5' },
         ],
     };
 
     onSortEnd = ({ oldIndex, newIndex }) => {
-        this.setState(({ tabItems }) => ({
-            tabItems: arrayMove(tabItems, oldIndex, newIndex),
-        }));
-        if (newIndex !== this.state.tabActiveIndex) {
-            this.setState({ tabActiveIndex: newIndex });
+        const { tabItems, tabActiveValue } = this.state;
+        const tabActiveIndex = tabItems.findIndex((item) => item.id === tabActiveValue);
+        const newTabItems = arrayMove(tabItems, oldIndex, newIndex);
+        this.setState(() => ({ tabItems: newTabItems.filter((item) => !!item) }));
+
+        if (oldIndex === newIndex) {
+            if (newIndex === tabItems.length) {
+                this.setState({ tabActiveValue: newTabItems[newIndex - 1].id });
+            } else {
+                this.setState({ tabActiveValue: newTabItems[newIndex].id });
+            }
+        } else {
+            this.setState({ tabActiveValue: newTabItems[newIndex].id });
         }
     };
 
-    renderTabContent = () => {
-        const { tabActiveIndex, tabContents } = this.state;
-        const activeTab = tabContents.find((item) => item.id === tabActiveIndex);
+    handleDelete = (event, id) => {
+        event.stopPropagation();
+        this.setState(({ tabActiveValue, tabItems }) => {
+            const tabActiveIndex = tabItems.findIndex((item) => item.id === tabActiveValue);
+            const removeItemIndex = tabItems.findIndex((item) => item.id === id);
+            tabItems = [...tabItems.slice(0, removeItemIndex), ...tabItems.slice(removeItemIndex + 1)];
 
+            if (removeItemIndex - 1 >= 0) {
+                if (tabActiveIndex === removeItemIndex) {
+                    tabActiveValue = tabItems[removeItemIndex - 1].id;
+                }
+            } else {
+                tabActiveValue = tabItems[0].id;
+            }
+
+            return { tabActiveValue, tabItems };
+        });
+    };
+
+    renderTabContent = () => {
+        const { tabActiveValue, tabItems } = this.state;
+        const activeTab = tabItems.find((item) => item.id === tabActiveValue);
         return (
-            <TabPanel value={tabActiveIndex} index={tabActiveIndex}>
-                {activeTab.title}
+            <TabPanel value={tabActiveValue} index={tabActiveValue}>
+                {activeTab?.title}
             </TabPanel>
         );
     };
 
     render() {
-        const { tabActiveIndex, tabItems } = this.state;
+        const { tabActiveValue, tabItems } = this.state;
 
         return (
             <div style={{ flexGrow: 1, width: '100%', backgroundColor: '#F5F5F5' }}>
                 <AppBar position="static" color="default">
-                    <DraggableTabContainer axis="x" lockAxis="x" value={tabActiveIndex} onSortEnd={this.onSortEnd} tabItems={tabItems} />
+                    <DraggableTabContainer
+                        axis="x"
+                        lockAxis="x"
+                        value={tabActiveValue}
+                        onSortEnd={this.onSortEnd}
+                        tabItems={tabItems}
+                        handleDelete={this.handleDelete}
+                    />
                 </AppBar>
                 {this.renderTabContent()}
             </div>
